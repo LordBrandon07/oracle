bd8b878f4bb83a4860cb2ae0a233a026bde65309
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-07-10 22:56:11 -0500

    clasedom05

--- DOMJS/script.js
+++ DOMJS/script.js
@@ -1,12 +1,34 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
+( () =>  {
+    
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card')
+    input.value = '';
+    const taskContent = document.createElement('div');
+    
+    const tittleTask = document.createElement('span');
+    tittleTask.classList.add('task');
+    tittleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(tittleTask);
+
+    //task.innerHTML = content;
+
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon())
+    list.appendChild(task);
+
 }
 
-console.log(btn);
+btn.addEventListener('click', createTask );
 
-
-btn.addEventListener('click', createTask )
+})();
