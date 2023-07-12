64978b1d80b7d2f6da34f4c5666cf2be3e94bf60
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-07-11 22:13:44 -0500

    formumalfidate

---
+++ FormulariosJS/js/validaciones.js
@@ -0,0 +1,24 @@
+console.log('viva colombia, viva falcao')
+
+const inputNacimiento = document.querySelector('#birth');
+
+inputNacimiento.addEventListener('blur', (evento) => {
+    validarNacimiento(evento.target);
+});
+
+function validarNacimiento(input){
+    const fechaCliente = new Date  (input.value);
+    let mensaje = '';
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje = 'No cumples con la edad minima'
+    }
+    mayorDeEdad(fechaCliente);
+    
+    input.setCustomValidity(mensaje)
+}
+
+function mayorDeEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(fecha.getUTCFullYear() + 18, fecha.getUTCMonth(), fecha.getUTCDate());
+    return fechaActual >= diferenciaFechas;
+}
