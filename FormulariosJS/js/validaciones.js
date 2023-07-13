7a359e83c84d13551421a2cbd1a626180c376794
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-07-12 22:49:13 -0500

    formumalfivali

--- FormulariosJS/js/validaciones.js
+++ FormulariosJS/js/validaciones.js
@@ -5,7 +5,30 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalid') 
+    }else{
+        input.parentElement.classList.add('input-container--invalid') 
+    }
 }
+
+const mensajesDeError = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+      valueMissing: "El campo correo no puede estar vacío",
+      typeMismatch: "El correo no es válido",
+    },
+    password: {
+      valueMissing: "El campo contraseña no puede estar vacío",
+      patternMismatch:"Mínimo ocho caracteres, al menos una letra y un número",
+    },
+    nacimiento: {
+      valueMissing: "Este campo no puede estar vacío",
+      customError: "Debes tener al menos 18 años de edad",
+    },
+  };
 
 const validadores = {
     nacimiento: input => validarNacimiento(input)
