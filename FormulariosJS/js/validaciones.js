2a6b71424ad20330c9759a7875e4d5191d586830
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-07-13 21:49:14 -0500

    formumalfitext

--- FormulariosJS/js/validaciones.js
+++ FormulariosJS/js/validaciones.js
@@ -7,10 +7,19 @@ export function valida(input){
     }
     if(input.validity.valid){
         input.parentElement.classList.remove('input-container--invalid') 
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
     }else{
         input.parentElement.classList.add('input-container--invalid') 
+        input.parentElement.querySelector('.input-message-error').innerHTML = mostrarMensajeDeError(tipoDeInput, input);
     }
 }
+
+const tipoDeErrores = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'valueMissing',
+]
 
 const mensajesDeError = {
     nombre: {
@@ -31,7 +40,17 @@ const mensajesDeError = {
   };
 
 const validadores = {
-    nacimiento: input => validarNacimiento(input)
+    nacimiento: input => validarNacimiento(input),
+}
+
+function mostrarMensajeDeError(tipoDeInput, input){
+  let mensaje = '';
+  tipoDeErrores.forEach(error =>{
+    if (input.validity[error]){
+      mensaje = mensajesDeError[tipoDeInput][error];
+    }
+  })
+  return mensaje;
 }
 
 function validarNacimiento(input){
