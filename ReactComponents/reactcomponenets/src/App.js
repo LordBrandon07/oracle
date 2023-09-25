a7da751433c0379871c68e5c17821e363a8751c4
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-09-24 23:40:54 -0500

    react4.1Fin

--- ReactComponents/reactcomponenets/src/App.js
+++ ReactComponents/reactcomponenets/src/App.js
@@ -5,13 +5,18 @@ import Typography from '@mui/material/Typography';
 
 
 function App() {
+
+  const handleSumbit = (valores) => {
+    console.log("appjs: ",valores)
+  }
+
   return (
     <Container 
       component="section"
       maxWidth="sm"
       >
       <Typography variant='h3' align='center' component="h1">Form</Typography>
-      <FormSignUp/> 
+      <FormSignUp handleSumbit={handleSumbit}/> 
     </Container>
     
   );
--- ReactComponents/reactcomponenets/src/components/FormSignUp.jsx
+++ ReactComponents/reactcomponenets/src/components/FormSignUp.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Switch from '@mui/material/Switch';
@@ -6,12 +6,32 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 
-function FormSignUp (){
+function FormSignUp ({handleSumbit}){
     const [name, setName] = useState("")
-    useEffect(() => {
-        console.log("new name: ", name)
-    }, [name])
-    return <form>
+    const [lastname, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [prom, setProm] = useState(true)
+    const [nov, setNov] = useState(false)
+
+    const [errors, setErrors] = useState({
+        name: {
+            error: false,
+            message: "minimo 3 caracteres",}
+
+    })
+
+    function validarNombre(nombre){
+        if(nombre.length >= 3){
+            return {name: {error: false, message: ""}}
+        }else{
+            return {name: {error: true, message: "minimo 3 caracteres"}}
+        }
+    }
+
+    return <form onSubmit={(e) => {
+        e.preventDefault()
+        handleSumbit({name, lastname, email, prom, nov})
+    }}>
         <TextField 
             id="name" 
             label="Nombre" 
@@ -19,36 +39,44 @@ function FormSignUp (){
             fullWidth 
             margin='normal'
             onChange={(e) => {
-                console.log(e.target.value)
                 setName(e.target.value)
             }}
-            value={name}/>    
+            value={name}
+            error={errors.name.error}
+            helperText={errors.name.error ? errors.name.message : ""}
+            onBlur={(e) => {
+                setErrors(validarNombre(e.target.value))
+            }}/>    
         <TextField 
             id="lastName" 
             label="Apellidos" 
             variant="outlined" 
             fullWidth 
-            margin='normal'/>
+            margin='normal'
+            value={lastname}
+            onChange={(e) => setLastName(e.target.value)}/>
         <TextField 
             id="email" 
             label="Email" 
             variant="outlined" 
             fullWidth 
-            margin='normal'/>
+            margin='normal'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
         <FormGroup>
             <FormControlLabel 
                 control={
-                    <Switch defaultChecked
+                    <Switch checked={prom} onChange={(e) => setProm(e.target.checked)}
                 />}
                 label="Promociones"/>
         
         <FormControlLabel 
                 control={
-                    <Switch defaultChecked
+                    <Switch checked={nov} onChange={(e) => setNov(e.target.checked)}
                 />}
                 label="Novedades"/>
         </FormGroup>
-        <Button variant="contained">Register</Button>
+        <Button variant="contained" type='sumbit'>Register</Button>
     </form>
 }
 
