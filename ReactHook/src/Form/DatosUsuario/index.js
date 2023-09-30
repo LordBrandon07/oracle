0c9ccb78157466ffbdda6c323cb199635f49a7f2
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-09-30 18:31:44 -0500

    react5

---
+++ ReactHook/src/Form/DatosUsuario/index.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+import { validarEmail, validarPassword } from "./validaciones";
+
+
+
+const DatosUsuario = () => {
+  const [email, setEmail] = useState({
+    value: "",
+    valid: null,
+  });
+  const [password, setPassword] = useState({ value: "", valid: null });
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     email: {
+  //       value: 'a',
+  //       valid: true,
+  //     },
+  //     password: {
+  //       value: 'b',
+  //       valid: true,
+  //     }
+  //   }
+  // }
+
+  
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (email.valid && password.valid) {
+          console.log("Siguiente formulario");
+          console.log(email, password);
+        } else {
+          console.log("No hacer nada");
+        }
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={email.valid === false}
+        helperText={
+          email.valid === false && "Ingresa un correo electrónico válido."
+        }
+        value={email.value}
+        onChange={(input) => {
+          const email = input.target.value;
+          const valido = validarEmail(email);
+          setEmail({ value: email, valid: valido });
+        }}
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        error={password.valid === false}
+        helperText={
+          password.valid === false &&
+          "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20."
+        }
+        value={password.value}
+        onChange={(input) => {
+          const password = input.target.value;
+          setPassword({ value: password, valid: validarPassword(password) });
+        }}
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
+};
+
+export default DatosUsuario;
